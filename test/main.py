4e3a6695b761f546bee0d8d7217f7a2618cf8647
Xsj234 <xushengjie@bupt.edu.cn>
2023-12-12 14:59:28 +0800

    增加了数据库

--- test/main.py
+++ test/main.py
@@ -1,6 +1,8 @@
 # 2023年12月2日
 # 徐胜杰
 # 登录界面主程序
+import psycopg2
+
 from InterfaceUi import *
 from LoginUi import *
 import webbrowser
@@ -8,7 +10,6 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 user_now = None
-
 
 
 class LoginWindow(QMainWindow):
@@ -32,17 +33,60 @@ class LoginWindow(QMainWindow):
         self.ui.pushButton_Register.clicked.connect(lambda: self.ui.stackedWidget_2.setCurrentIndex(1))
         # 登录界面跳转主界面
         self.ui.pushButton_L_sure.clicked.connect(self.loginIn)
+
+        self.ui.pushButton_R_sure.clicked.connect(self.registerIn)
         self.show()
 
     def loginIn(self):
         # 判断账号密码 报警和跳转
         account = self.ui.lineEdit_L_account.text()
         password = self.ui.lineEdit_L_password.text()
-        if account == 'admin' and password == "123456":
-            self.win = MainWindow()
-            self.close()
+        account_list = []
+        password_list = []
+        conn = psycopg2.connect(database="DataMy", user="postgres", password="123456", port="5432")
+        cur = conn.cursor()
+        # 数据库语言
+        cur.execute("select * from users;")
+        rows = cur.fetchall()
+        for row in rows:
+            account_list.append(row[0])
+            password_list.append(row[1])
+        print(account_list, password_list)
+        # 上传
+        conn.commit()
+        # 关闭
+        conn.close()
+
+        for i in range(len(account_list)):
+            if len(account) == 0 or len(password) == 0:
+                self.ui.stackedWidget.setCurrentIndex(1)
+            elif account == account_list[i] and password == password_list[i]:
+                global user_now
+                user_now = account
+                print(user_now)
+                self.win = MainWindow()
+                self.close()
+            else:
+                self.ui.stackedWidget.setCurrentIndex(2)
+
+    def registerIn(self):
+        account = self.ui.lineEdit_R_account.text()
+        password = self.ui.lineEdit__R_password_1.text()
+        password2 = self.ui.lineEdit__R_password_2.text()
+        if len(account)==0 or len(password)==0 or len(password2)==0:
+            self.ui.stackedWidget.setCurrentIndex(1)
+        elif password!=password2:
+            self.ui.stackedWidget.setCurrentIndex(3)
         else:
-            print("wrong")
+            self.ui.stackedWidget.setCurrentIndex(4)
+            conn = psycopg2.connect(database="DataMy", user="postgres", password="123456", port="5432")
+            cur = conn.cursor()
+            # 数据库语言
+            cur.execute(f"insert into users values('{account}','{password}');")
+            # 上传
+            conn.commit()
+            # 关闭
+            conn.close()
 
 
 class MainWindow(QMainWindow):
@@ -65,7 +109,7 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_web.clicked.connect(self.goWeb)
         self.ui.pushButton_my.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
         self.ui.pushButton_logout.clicked.connect(self.logout)
-
+        self.ui.pushButton_M_sure.clicked.connect(self.changePassword)
         self.show()
 
     def goWeb(self):
@@ -81,6 +125,24 @@ class MainWindow(QMainWindow):
         self.login = LoginWindow()
         self.close()
         user_now = ''
+
+    def changePassword(self):
+        global user_now
+        password = self.ui.lineEdit_L_M_pass1.text()
+        if len(self.ui.lineEdit_L_M_pass1.text()) == 0 or len(self.ui.lineEdit_L_M_pass2.text()) == 0:
+            self.ui.stackedWidget_2.setCurrentIndex(1)
+        elif self.ui.lineEdit_L_M_pass1.text() == self.ui.lineEdit_L_M_pass2.text():
+            conn = psycopg2.connect(database="DataMy", user="postgres", password="123456", port="5432")
+            cur = conn.cursor()
+            # 数据库语言
+            cur.execute(f"update users set passwords='{password}' where accounts = '{user_now}';")
+            conn.commit()
+            # 关闭
+            conn.close()
+            self.ui.stackedWidget_2.setCurrentIndex(3)
+        else:
+            self.ui.stackedWidget_2.setCurrentIndex(2)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
